c7f6e69df9fbf1be4de0635dc6af2268b21dab5c
Michael Markus Ackermann <michael_ackermann@outlook.com>
2020-03-20 18:58:06 -0300

    [ADDED] Documentation for Rayleigh
    This is the inital commit of the documentation commits.
    Added docstring to the "rayleigh" and "rayleigh_absorption" functions.

--- absorption_models.py
+++ absorption_models.py
@@ -80,6 +80,30 @@ def delany_bazley_absorption(zc, kc, d, c_imp_air):
 
 
 def rayleigh(freq, flow_resis, air_dens, sound_spd, poros):
+    """
+    Returns through the Rayleigh Model the Material Charactheristic Impedance
+    and the Material Wave Number.
+
+        Parameters:
+        ----------
+            freq : ndarray
+                A range of frequencies
+            flow_resis : int
+                Resistivity of the material
+            air_dens : int | float
+                The air density
+            sound_spd : int | float
+                The speed of the sound
+            poros : float
+                Porosity of the material
+
+        Returns:
+        -------
+            zc : int | float | complex
+                Material Charactheristic Impedance
+            kc : int | float | complex
+                Material Wave Number
+    """
     omega = 2 * np.pi * freq
     alpha = (1 - (1j * poros * flow_resis) / (air_dens * omega)) ** 0.5
     kc = (omega/sound_spd) * alpha
@@ -88,6 +112,26 @@ def rayleigh(freq, flow_resis, air_dens, sound_spd, poros):
 
 
 def rayleigh_absorption(zc, kc, d, c_imp_air):
+    """
+    Returns the Sound Absorption Coefficient for the Rayleigh Model.
+    NOTE: Only use it with the rayleigh function.
+
+        Parameters:
+        -----------
+            zc : int | float | complex
+                Material Charactheristic Impedance
+            kc : int | float | complex
+                Material Wave Number
+            d : float
+                Material Thickness
+            c_imp_air : int | float
+                Air Characteristic Impedance
+
+        Returns:
+        --------
+            absorption : int | ndarray
+                Sound Absorption Coefficient of the Material
+    """
     zs = -1j * (zc / np.tan(kc * d))
     reflex = (zs - c_imp_air) / (zs + c_imp_air)
     absorption = 1 - np.abs(reflex) ** 2
