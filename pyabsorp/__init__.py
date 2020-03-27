7c787cd410e02263e97ee34e0c04e882a92860a1
Michael Markus Ackermann <michael_ackermann@outlook.com>
2020-03-27 18:08:37 -0300

    [ADDED] the __init__ files
    Added the __init__ files with the basic code.

---
+++ pyabsorp/__init__.py
@@ -0,0 +1,27 @@
+"""
+
+=================
+@Author: Michael Markus Ackermann
+"""
+
+from air import air_properties
+
+from models import delany_bazley, delany_bazley_absorption, \
+    rayleigh, rayleigh_absorption, biot_allard, biot_allard_absorption, \
+    johnson_champoux, johnson_champoux_absorption
+
+__author__ = "Michael Markus Ackermann"
+__date__ = "27 March 2020"
+__version__ = "1.0.0"
+
+# package submodules and scripts to be called as pyabsorp.something
+__all__ = [  # Functions
+            'air_properties',
+            'delany_bazley',
+            'delany_bazley_absorption',
+            'rayleigh',
+            'rayleigh_absorption',
+            'biot_allard',
+            'biot_allard_absorption',
+            'johnson_champoux',
+            'johnson_champoux_absorption']
