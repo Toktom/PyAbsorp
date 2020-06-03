f4b8c596003729a95a6a33ffeda3ae25b5fc7f5f
Michael Markus Ackermann <michael_ackermann@outlook.com>
2020-06-03 16:38:22 -0300

    [FIXED] wrong information in comments and documentation
    Fixed some wrong information in comments and documentation

--- pyabsorp/air.py
+++ pyabsorp/air.py
@@ -12,43 +12,43 @@ def air_properties(temp, hum, atm):
     Parameters:
     -----------
         temp: int | float
-            Air temperatue
+            Air temperatue (Celsius)
         hum : int | float
-            Air humidity
+            Air humidity (%)
         atm : int | float
-            Atmospheric pressure
+            Atmospheric pressure (Pa)
 
     Returns:
     --------
         sound_spd: int | float
-            The speed of the sound in the air
+            The speed of the sound in the air (m/s)
         air_dens: int | float
-            The air density
+            The air density (kg/m³)
         z_air: int | float
             Characteristic Impedance of the Air
         viscos: int | float
-            The dynamic vicosity of the air (a.k.a. neta (greek letter))
+            The dynamic vicosity of the air (a.k.a. neta (greek letter))(Ns/m²)
         expans: int | float
-            Ratio of specific heat
+            Specific heat ratio (no units)
         prandtl: int | float
-            Prandtl's number
+            Prandtl's number (fewly varies at typical air conditions)(no units)
         Cp: int | float
-            Specific heat capacity
+           Constant Pressure Spectfic Heat (J/kg·K)
     """
     KAPPA = 0.026  # W/(mK) air
-    AIR_CONST = 287.031  # J/K/kg
-    WATER_CONST = 461.521  # J/K/kg
-    temp += 273.16  # K#
+    AIR_CONST = 287.031  # Gas constant for air (J/K/kg)
+    WATER_CONST = 461.521  # Gas constant for water vapor (J/K/kg)
+    temp += 273.16  # Converts the temperature from Ceilsius to Kelvin
     pierce = 0.0658 * temp**3 - 53.7558 * temp**2 \
-        + 14703.8127 * temp - 1345485.0465
+        + 14703.8127 * temp - 1345485.0465  # Pierce
     viscos = 7.72488e-8 * temp - 5.95238e-11 * temp**2 \
-        + 2.71368e-14 * temp**3
+        + 2.71368e-14 * temp**3  # Absolute (or dynamic) viscosity
     Cp = 4168.8 * (0.249679 - 7.55179e-5 * temp
                    + 1.69194e-7 * temp**2 - 6.46128e-11 * temp**3)
-    # Volume Specific Heat Constant (J/kg/K) for 260 K < T < 600 K
-    vol_Const = Cp - AIR_CONST
-    prandtl = viscos * Cp / KAPPA  # prandtl number
-    expans = Cp / vol_Const  # k
+    # Constant Pressure Spectfic Heat
+    Cv = Cp - AIR_CONST  # Constant Volume Specific Heat (J/kg/K) for 260 K < T < 600 K
+    prandtl = viscos * Cp / KAPPA  # Prandtl number
+    expans = Cp / Cv  # Specific heat ratio
     air_dens = atm / (AIR_CONST * temp) - (1 / AIR_CONST - 1 / WATER_CONST) * \
         hum / 100 * pierce / temp  # Air density
     sound_spd = (expans * atm / air_dens) ** 0.5  # Speed of the sound
