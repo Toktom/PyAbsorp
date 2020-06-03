55da5603f03bcd995d726d9fbf066eff47a1bd0d
Michael Markus Ackermann <michael_ackermann@outlook.com>
2020-06-03 16:18:55 -0300

    [CHANGED] Updated init files and version
    Updated the '__init__.py" files due the previous changes and changed the 'version.py' file.

--- pyabsorp/__init__.py
+++ pyabsorp/__init__.py
@@ -3,10 +3,9 @@ PYABSORP
 =====================================================
 Author:
     Michael Markus Ackermann - dev.toktom@outlook.com
-    
 PyAbsorp:
     This is a package developed to be use to find the Sound Absorption
-    Coefficient through some implemented models, like  Biot-Allard, 
+    Coefficient through some implemented models, like  Biot-Allard,
     Johnson-Champoux and others.
     In order to provide such functionalities we require a few packages
     to be installed:
@@ -21,21 +20,17 @@ documentation.
 """
 
 from pyabsorp.air import air_properties
+from pyabsorp.absorption import absorption_coefficient
 from pyabsorp.version import __author__, __date__, __version__
-
-from pyabsorp.models import delany_bazley, delany_bazley_absorption, \
-    rayleigh, rayleigh_absorption, biot_allard, biot_allard_absorption, \
-    johnson_champoux, johnson_champoux_absorption
+from pyabsorp.models import delany_bazley, rayleigh, biot_allard, \
+    johnson_champoux
 
 
 # package submodules and scripts to be called as pyabsorp.something
 __all__ = [  # Functions
+            'absorption_coefficient',
             'air_properties',
             'delany_bazley',
-            'delany_bazley_absorption',
             'rayleigh',
-            'rayleigh_absorption',
             'biot_allard',
-            'biot_allard_absorption',
-            'johnson_champoux',
-            'johnson_champoux_absorption']
+            'johnson_champoux']
--- pyabsorp/models/__init__.py
+++ pyabsorp/models/__init__.py
@@ -13,30 +13,22 @@ Models:
     Available functions:
     ---------------------
 
-        >>> pyabsorp.delany_bazley(flow_resis, air_dens, ..., var='default')
-        >>> pyabsorp.delany_bazley_absorption(zc, kc, d, z_air)
+        >>> pyabsorp.delany_bazley(flow_resis, air_dens, ..., var='default')#
         >>> pyabsorp.rayleigh(flow_resis, air_dens, ..., freq)
-        >>> pyabsorp.rayleigh_absorption(zc, kc, d, z_air)
         >>> pyabsorp.biot_allard(flow_resis, air_dens, ..., freq)
-        >>> pyabsorp.biot_allard_absorption(zc, kc, d, z_air)
         >>> pyabsorp.johnson_champoux(flow_resis, air_dens, ... var='default')
-        >>> pyabsorp.johnson_champoux_absorption(zc, kc, d, z_air)
 
 
     For further information, check the function specific documentation.
 """
 
-from .delany_bazley import delany_bazley, delany_bazley_absorption
-from .rayleigh import rayleigh, rayleigh_absorption
-from .biot_allard import biot_allard, biot_allard_absorption
-from .johnson_champoux import johnson_champoux, johnson_champoux_absorption
+from .delany_bazley import delany_bazley
+from .rayleigh import rayleigh
+from .biot_allard import biot_allard
+from .johnson_champoux import johnson_champoux
 
 __all__ = [  # Functions
             'delany_bazley',
-            'delany_bazley_absorption',
             'rayleigh',
-            'rayleigh_absorption',
             'biot_allard',
-            'biot_allard_absorption',
-            'johnson_champoux',
-            'johnson_champoux_absorption']
+            'johnson_champoux']
