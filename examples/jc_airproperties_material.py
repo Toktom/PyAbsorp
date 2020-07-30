c4f55cc36a32ccabd8925beaef8cec33545544b5
Chum4k3r <joao.paes@eac.ufsm.br>
2020-07-30 01:32:33 -0300

    [DOCS] Added examples using the new classes.

---
+++ examples/jc_airproperties_material.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Example script to estimate a material absorption coeficient.
+Using AirProperties and Material classes within same Johnson-Champoux example code.
+
+Created on Thu Jul 30 00:55:08 2020
+
+@author: joaovitor
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+import pyabsorp as ab  # Must be in the same folder as this file
+
+
+# Using new AirProperties class
+air = ab.AirProperties(temp = 25,           # Temperature
+                       hum = 30,            # Relative humidity
+                       atm = 101320)        # Atmospherical pressure
+
+
+# Using new Material class
+mat = ab.Material(thick = 0.05,                             # Thickness
+                  freq = np.arange(100, 10001, 1),          # Frequencies
+                  air = air,                                # Air properties
+                  flowres = 35000,                          # Static flow resistivity
+                  poros = 0.65,                             # Open porosity
+                  tortus = 1.,                              # Tortuosity
+                  visclen = 750e-5,                         # Viscous characteristic length
+                  thermlen = 500e-5,                        # Thermal characteristic length
+                  thermperm = ((0.65 / 8) * (500e-5)**2))   # Static thermal permeability
+
+
+variations = ['default', 'allard', 'lafarge']
+
+
+legends = ['\u03B1 Johnson-Chmapoux',
+           '\u03B1 Johnson-Chmapoux-Allard',
+           '\u03B1 Johnson-Chmapoux-Allard-Lafarge']
+
+
+plt.title("Sound Absorption Coefficient Chart")
+
+for idx in range(3):
+    # Calculate the absorption for a given variation
+    mat.estimate_absorption(method='jc', var=variations[idx])
+
+    # Draw the lines
+    plt.semilogx(mat.frequencies, 100*mat.absorption, label=legends[idx])
+
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Absorption Coefficient [%]')
+plt.legend()
+plt.grid(True, which="both", ls="-")
+plt.show()
