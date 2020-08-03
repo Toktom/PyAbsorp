b8cc40fbe02087cca41674685c3365b680361e89
Michael Markus Ackermann <michael_ackermann@outlook.com>
2020-08-03 09:46:10 -0300

    [CHANGED] version.py
    Changed the version from 0.1.1b to 0.1.2

--- pyabsorp/version.py
+++ pyabsorp/version.py
@@ -1,4 +1,4 @@
 # separeted file to get version information without importing pyabsorp lib
 __author__ = "Michael Markus Ackermann"
-__date__ = "03 March 2020"
-__version__ = "0.1.1b"
+__date__ = "03 August 2020"
+__version__ = "0.1.2"
